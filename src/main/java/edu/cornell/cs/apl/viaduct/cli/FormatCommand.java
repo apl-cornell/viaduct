30265d85a200c3f330c07357a424d268cfcca82e
Rolph Recto <rolph.recto@gmail.com>
2019-06-22 13:51:48 -0400

    type error fix

--- src/main/java/edu/cornell/cs/apl/viaduct/cli/FormatCommand.java
+++ src/main/java/edu/cornell/cs/apl/viaduct/cli/FormatCommand.java
@@ -1,7 +1,7 @@
 package edu.cornell.cs.apl.viaduct.cli;
 
 import com.github.rvesse.airline.annotations.Command;
-import edu.cornell.cs.apl.viaduct.imp.ast.ImpAstNode;
+import edu.cornell.cs.apl.viaduct.imp.ast.ProgramNode;
 import edu.cornell.cs.apl.viaduct.imp.visitors.PrintVisitor;
 import java.io.BufferedWriter;
 
@@ -9,7 +9,7 @@ import java.io.BufferedWriter;
 public class FormatCommand extends BaseCommand {
   @Override
   public Void call() throws Exception {
-    final ImpAstNode program = this.input.parse();
+    final ProgramNode program = this.input.parse();
     try (BufferedWriter writer = this.output.newOutputWriter()) {
       writer.write(new PrintVisitor().run(program));
       writer.newLine();
--- src/test/java/edu/cornell/cs/apl/viaduct/imp/visitors/PrintVisitorTest.java
+++ src/test/java/edu/cornell/cs/apl/viaduct/imp/visitors/PrintVisitorTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.cornell.cs.apl.viaduct.ExamplesProvider;
 import edu.cornell.cs.apl.viaduct.ImpAstParser;
-import edu.cornell.cs.apl.viaduct.imp.ast.ImpAstNode;
+import edu.cornell.cs.apl.viaduct.imp.ast.ProgramNode;
 import edu.cornell.cs.apl.viaduct.imp.parser.Parser;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.converter.ConvertWith;
@@ -13,8 +13,8 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 class PrintVisitorTest {
   @ParameterizedTest
   @ArgumentsSource(ExamplesProvider.class)
-  void testDualToParser(@ConvertWith(ImpAstParser.class) ImpAstNode ast) throws Exception {
-    final String printedAst = new PrintVisitor().run(ast);
-    assertEquals(ast, Parser.parse(printedAst));
+  void testDualToParser(@ConvertWith(ImpAstParser.class) ProgramNode prog) throws Exception {
+    final String printedAst = new PrintVisitor().run(prog);
+    assertEquals(prog, Parser.parse(printedAst));
   }
 }

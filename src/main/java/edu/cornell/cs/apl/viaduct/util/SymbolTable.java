7080afa88955ac5c4cd292b7261feebc35322bdb
Cosku Acay <coskuacay@gmail.com>
2019-08-08 12:52:29 -0400

    Add documentation to SymbolTable

--- src/main/java/edu/cornell/cs/apl/viaduct/util/SymbolTable.java
+++ src/main/java/edu/cornell/cs/apl/viaduct/util/SymbolTable.java
@@ -6,10 +6,10 @@ import java.util.NoSuchElementException;
 import java.util.Stack;
 
 /** A generic data structure for a stack of maps, useful for maintaining lexical scoping. */
-public final class SymbolTable<V, T> implements Cloneable {
-  private final Stack<Map<V, T>> tableStack;
+public final class SymbolTable<K, V> implements Cloneable {
+  private final Stack<Map<K, V>> tableStack;
 
-  private SymbolTable(Stack<Map<V,T>> tableStack) {
+  private SymbolTable(Stack<Map<K, V>> tableStack) {
     this.tableStack = tableStack;
   }
 
@@ -18,39 +18,42 @@ public final class SymbolTable<V, T> implements Cloneable {
     this.tableStack.push(HashMap.empty());
   }
 
-  public boolean contains(V var) {
-    return this.tableStack.peek().containsKey(var);
-  }
-
-  /** Get the value associated with the given in the current (i.e. topmost/innermost) scope. */
-  public T get(V var) {
-    return this.tableStack
-        .peek()
-        .get(var)
-        .getOrElseThrow(() -> new NoSuchElementException(var.toString()));
-  }
-
-  public void add(V var, T data) {
-    this.tableStack.push(this.tableStack.pop().put(var, data));
-  }
-
-  public void push() {
-    this.tableStack.push(this.tableStack.peek());
-  }
-
-  public void pop() {
-    this.tableStack.pop();
-  }
-
   public void clear() {
     this.tableStack.clear();
     this.tableStack.push(HashMap.empty());
   }
 
+  public boolean contains(K key) {
+    return this.tableStack.peek().containsKey(key);
+  }
+
+  /** Get the value associated with the given key in the current (i.e. topmost/innermost) scope. */
+  public V get(K key) {
+    return this.tableStack
+        .peek()
+        .get(key)
+        .getOrElseThrow(() -> new NoSuchElementException(key.toString()));
+  }
+
+  /** Add the given key with the given value to the current scope. */
+  public void add(K key, V value) {
+    this.tableStack.push(this.tableStack.pop().put(key, value));
+  }
+
+  /** Add a new scope. The new scope contains all mappings in previous scopes. */
+  public void push() {
+    this.tableStack.push(this.tableStack.peek());
+  }
+
+  /** Remove the most recently added scope. */
+  public void pop() {
+    this.tableStack.pop();
+  }
+
   @Override
   public Object clone() throws CloneNotSupportedException {
-    Stack<Map<V,T>> tableStack = (Stack<Map<V,T>>)this.tableStack.clone();
-    return new SymbolTable<V,T>(tableStack);
+    Stack<Map<K, V>> tableStack = (Stack<Map<K, V>>) this.tableStack.clone();
+    return new SymbolTable<>(tableStack);
   }
 
   @Override

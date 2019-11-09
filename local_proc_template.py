f5b2fcdadcff4bca63ffb9210969a97ef22dec3a
Rolph Recto <rolph.recto@gmail.com>
2019-11-09 17:31:51 -0500

    python scripts for mamba runtime

---
+++ local_proc_template.py
@@ -0,0 +1,46 @@
+next_base = MemValue(regint(0))
+
+def array_alloc(size):
+  global next_base
+  arr_base = next_base.read()
+  next_base.write(next_base.read() + size)
+  return arr_base
+
+
+def secret_load(arr, i):
+  val = sregint.load_mem(arr + i)
+  return val
+
+
+def clear_load(arr, i):
+  val = regint.load_mem(arr + i)
+  return val
+
+
+def secret_store(arr, i, v):
+  v.store_in_mem(arr + i)
+
+
+def clear_store(arr, i, v):
+  v.store_in_mem(arr + i)
+
+
+def get_input(n):
+  input = sint.get_private_input_from(n)
+  input_reg = sregint()
+  input_reg.load_secret(input)
+  return input_reg
+
+
+def send_secret_output(sreg, player_num):
+  sval = sint()
+  sval.load_secret(sreg)
+  sval.reveal_to(player_num)
+
+
+def send_clear_output(reg, player_num):
+  val = cint(reg)
+  val.public_output(player_num)
+
+
+obj = {}
---
+++ viaduct_player.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from Queue import Queue
+import threading
+import subprocess
+import sys
+import os
+
+
+class MambaOutThread (threading.Thread):
+  def __init__(self, mamba_proc, in_queue):
+    threading.Thread.__init__(self)
+    self.mamba_proc = mamba_proc
+    self.in_queue = in_queue
+
+  def get_lines(self):
+    for line in iter(self.mamba_proc.stdout.readline, ""):
+      yield line
+
+  def run(self):
+    for line in self.get_lines():
+      if "VIADUCT_OUTPUT" in line:
+        self.in_queue.put(int(line.split()[1]), block=False)
+
+
+player_num  = sys.argv[1]
+program     = sys.argv[2]
+
+mamba_proc = \
+    subprocess.Popen(["./Player.x", player_num, program], \
+        stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+in_queue = Queue()
+mamba_thread = MambaOutThread(mamba_proc, in_queue)
+
+
+def get_user_input(var):
+  print "input", var, ":",
+  x = raw_input()
+  return x
+
+
+def send_mamba_input(val):
+  mamba_proc.stdin.write(val + "\n")
+
+
+def get_mamba_output():
+  val = in_queue.get()
+  return val
+
+
+def send_user_output(val):
+  print "output:", val
+
+
+mamba_thread.start()
+
+local_proc = os.path.join(program, "player_{}.py".format(player_num))
+execfile(local_proc)
+
+mamba_thread.join()
+

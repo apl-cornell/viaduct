3c6c440a5a556145a30f052ca58151470a30a641
Rolph Recto <rolph.recto@gmail.com>
2019-11-12 14:41:40 -0500

    added profiling

--- viaduct_player.py
+++ viaduct_player.py
@@ -8,10 +8,11 @@ import os
 
 
 class MambaOutThread (threading.Thread):
-  def __init__(self, mamba_proc, in_queue):
+  def __init__(self, mamba_proc, in_queue, prof_data):
     threading.Thread.__init__(self)
     self.mamba_proc = mamba_proc
     self.in_queue = in_queue
+    self.prof_data = prof_data
 
   def get_lines(self):
     for line in iter(self.mamba_proc.stdout.readline, ""):
@@ -22,17 +23,46 @@ class MambaOutThread (threading.Thread):
       if "VIADUCT_OUTPUT" in line:
         self.in_queue.put(int(line.split()[1]), block=False)
 
+      elif "VIADUCT_PROFILE_NETWORK" in line:
+        data = line.split()
+        player = data[1]
+        thread_num = data[2]
+        data_sent = int(data[3])
+        data_received = int(data[4])
+
+        if player not in prof_data["network"]:
+          prof_data["network"][player] = {}
+
+        prof_data["network"][player][thread_num] = {
+          "sent": data_sent,
+          "received": data_received
+        }
+
+      elif "VIADUCT_PROFILE_RESOURCE" in line:
+        data = line.split()
+        player = data[1]
+        thread_num = data[2]
+        process_num = data[3]
+        maxrss = int(data[4])
+
+        if player not in prof_data["resource"]:
+          prof_data["resource"][player] = {}
+
+        prof_data["resource"][player][thread_num] = {
+          "maxrss": maxrss
+        }
+        
 
 player_num  = sys.argv[1]
 program     = sys.argv[2]
+prof_data   = { "network": {}, "resource": {} }
 
 mamba_proc = \
     subprocess.Popen(["./Player.x", player_num, program], \
         stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
 in_queue = Queue()
-mamba_thread = MambaOutThread(mamba_proc, in_queue)
-
+mamba_thread = MambaOutThread(mamba_proc, in_queue, prof_data)
 
 def user_input(varname, var):
   print "input", varname, ":",
@@ -75,10 +105,32 @@ def user_output(val):
   print "output:", val
 
 
+def print_profile_info(data):
+  for player, player_data in data["network"].iteritems():
+    bytes_sent = 0
+    bytes_received = 0
+
+    for thread, thread_data in player_data.iteritems():
+      bytes_sent += thread_data["sent"]
+      bytes_received += thread_data["received"]
+
+    print "player {}, bytes sent: {}, bytes received: {}".format(player, bytes_sent, bytes_received)
+
+  for player, player_data in data["resource"].iteritems():
+    maxrss = 0
+
+    for thread, thread_data in player_data.iteritems():
+      maxrss += thread_data["maxrss"]
+
+    print "player {}, maxrss: {}".format(player, maxrss)
+
+
 mamba_thread.start()
 
 local_proc = os.path.join(program, "player_{}.py".format(player_num))
 execfile(local_proc)
 
 mamba_thread.join()
+
+print_profile_info(prof_data)
 

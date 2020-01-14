89d58c278ad4df6bb615fd4cccd7b85d349d453d
Cosku Acay <coskuacay@gmail.com>
2020-01-14 16:55:42 -0500

    Turn input/receive statements into expressions in the surface

--- src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/surface/Statements.kt
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/surface/Statements.kt
@@ -117,54 +117,16 @@ data class BlockNode(
 
 // Communication Statements
 
-/** Statements that perform external input/output. */
-sealed class IOStatementNode : StatementNode() {
-    /** Host to input from or output to. */
-    abstract val host: HostNode
-}
-
-/**
- * An external input.
- *
- * @param temporary Store the received value into this variable.
- * @param type Type of the value to receive.
- */
-data class InputNode(
-    val temporary: TemporaryNode,
-    val type: ValueTypeNode,
-    override val host: HostNode,
-    override val sourceLocation: SourceLocation
-) : IOStatementNode()
-
 /** An external output. */
 data class OutputNode(
     val message: ExpressionNode,
-    override val host: HostNode,
+    val host: HostNode,
     override val sourceLocation: SourceLocation
-) : IOStatementNode()
-
-/** Statements that send messages to or receive messages from other protocols. */
-sealed class CommunicationStatementNode : StatementNode() {
-    /** Protocol to receive from or send to. */
-    abstract val protocol: ProtocolNode
-}
-
-/**
- * Receiving a value from another protocol.
- *
- * @param temporary Store the received value into this variable.
- * @param type Type of the value to receive.
- */
-data class ReceiveNode(
-    val temporary: TemporaryNode,
-    val type: ValueTypeNode,
-    override val protocol: ProtocolNode,
-    override val sourceLocation: SourceLocation
-) : CommunicationStatementNode()
+) : StatementNode()
 
 /** Sending a value to another protocol. */
 data class SendNode(
     val message: ExpressionNode,
-    override val protocol: ProtocolNode,
+    val protocol: ProtocolNode,
     override val sourceLocation: SourceLocation
-) : CommunicationStatementNode()
+) : StatementNode()

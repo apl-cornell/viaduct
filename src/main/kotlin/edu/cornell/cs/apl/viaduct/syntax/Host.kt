8466311da781f3907db10d1d6d072ec5f4b02e4c
Cosku Acay <coskuacay@gmail.com>
2020-01-23 14:15:59 -0500

    Make Host comparable

--- src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/Host.kt
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/Host.kt
@@ -6,7 +6,10 @@ package edu.cornell.cs.apl.viaduct.syntax
  * Hosts have inputs and outputs.
  * Hosts are location that can run (one or more) processes.
  */
-data class Host(override val name: String) : Name {
+data class Host(override val name: String) : Name, Comparable<Host> {
     override val nameCategory: String
         get() = "host"
+
+    override fun compareTo(other: Host): Int =
+        this.name.compareTo(other.name)
 }

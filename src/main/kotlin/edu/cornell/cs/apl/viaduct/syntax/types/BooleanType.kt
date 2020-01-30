e1dc38a0a47512e8eda1949e69bb412bcfe9ef1e
Cosku Acay <coskuacay@gmail.com>
2020-01-30 12:11:01 -0500

    Add default values for value types

--- src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/BooleanType.kt
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/BooleanType.kt
@@ -1,7 +1,13 @@
 package edu.cornell.cs.apl.viaduct.syntax.types
 
+import edu.cornell.cs.apl.viaduct.syntax.values.BooleanValue
+import edu.cornell.cs.apl.viaduct.syntax.values.Value
+
 /** The type of booleans. */
 object BooleanType : ValueType {
+    override val defaultValue: Value
+        get() = BooleanValue(false)
+
     override fun toString(): String {
         return "bool"
     }
--- src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/IntegerType.kt
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/IntegerType.kt
@@ -1,7 +1,13 @@
 package edu.cornell.cs.apl.viaduct.syntax.types
 
+import edu.cornell.cs.apl.viaduct.syntax.values.IntegerValue
+import edu.cornell.cs.apl.viaduct.syntax.values.Value
+
 /** The type of integers. */
 object IntegerType : ValueType {
+    override val defaultValue: Value
+        get() = IntegerValue(0)
+
     override fun toString(): String {
         return "int"
     }

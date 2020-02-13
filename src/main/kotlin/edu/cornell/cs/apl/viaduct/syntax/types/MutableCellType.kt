97dd275405eb047c9b9f3326723aac291ba0af39
Cosku Acay <coskuacay@gmail.com>
2020-02-12 23:14:01 -0500

    Use pattern matching in ObjectTypes instead of Maps since this is a lot more space efficient

--- src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/MutableCellType.kt
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/MutableCellType.kt
@@ -2,34 +2,35 @@ package edu.cornell.cs.apl.viaduct.syntax.types
 
 import edu.cornell.cs.apl.viaduct.security.Label
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.Get
+import edu.cornell.cs.apl.viaduct.syntax.datatypes.Modify
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.QueryName
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.Set
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.UpdateName
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.persistentMapOf
 
 /** The type of a mutable cell. */
 data class MutableCellType(val elementType: ValueType, val elementLabel: Label?) :
     ObjectType {
-    private val queries =
-        persistentMapOf(
-            Pair(Get, FunctionType(result = elementType))
-        )
-
-    private val updates =
-        persistentMapOf(
-            Pair(Set, FunctionType(elementType, result = UnitType))
-        )
-
     override val constructorArguments: List<ValueType>
         get() = persistentListOf(elementType)
 
     override fun getType(query: QueryName): FunctionType? {
-        return queries[query]
+        return when (query) {
+            is Get ->
+                FunctionType(result = elementType)
+            else ->
+                null
+        }
     }
 
     override fun getType(update: UpdateName): FunctionType? {
-        // TODO: Pair(Modify, UpdateInfo(persistentListOf(IntegerType, elementType)))
-        return updates[update]
+        return when (update) {
+            is Set ->
+                FunctionType(elementType, result = UnitType)
+            is Modify ->
+                TODO()
+            else ->
+                null
+        }
     }
 }
--- src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/VectorType.kt
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/syntax/types/VectorType.kt
@@ -2,34 +2,35 @@ package edu.cornell.cs.apl.viaduct.syntax.types
 
 import edu.cornell.cs.apl.viaduct.security.Label
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.Get
+import edu.cornell.cs.apl.viaduct.syntax.datatypes.Modify
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.QueryName
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.Set
 import edu.cornell.cs.apl.viaduct.syntax.datatypes.UpdateName
 import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.persistentMapOf
 
 /** The type of a vector. */
 data class VectorType(val elementType: ValueType, val elementLabel: Label?, val sizeLabel: Label?) :
     ObjectType {
-    private val queries =
-        persistentMapOf(
-            Pair(Get, FunctionType(IntegerType, result = elementType))
-        )
-
-    private val updates =
-        persistentMapOf(
-            Pair(Set, FunctionType(IntegerType, elementType, result = UnitType))
-        )
-
     override val constructorArguments: List<ValueType>
         get() = persistentListOf(IntegerType)
 
     override fun getType(query: QueryName): FunctionType? {
-        return queries[query]
+        return when (query) {
+            is Get ->
+                FunctionType(IntegerType, result = elementType)
+            else ->
+                null
+        }
     }
 
     override fun getType(update: UpdateName): FunctionType? {
-        // TODO: Pair(Modify, UpdateInfo(persistentListOf(IntegerType, elementType)))
-        return updates[update]
+        return when (update) {
+            is Set ->
+                FunctionType(IntegerType, elementType, result = UnitType)
+            is Modify ->
+                TODO()
+            else ->
+                null
+        }
     }
 }

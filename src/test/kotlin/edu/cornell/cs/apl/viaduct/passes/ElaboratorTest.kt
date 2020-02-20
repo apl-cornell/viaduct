d92cf3ea19be5d3f9bb61c9b3684fac0cf8e7e63
Cosku Acay <coskuacay@gmail.com>
2020-02-20 16:51:53 -0500

    Add elaborator test

---
+++ src/test/kotlin/edu/cornell/cs/apl/viaduct/passes/ElaboratorTest.kt
@@ -0,0 +1,14 @@
+package edu.cornell.cs.apl.viaduct.passes
+
+import edu.cornell.cs.apl.viaduct.ExampleProgramProvider
+import edu.cornell.cs.apl.viaduct.syntax.surface.ProgramNode
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ArgumentsSource
+
+internal class ElaboratorTest {
+    @ParameterizedTest
+    @ArgumentsSource(ExampleProgramProvider::class)
+    fun `it elaborates`(program: ProgramNode) {
+        program.elaborate()
+    }
+}

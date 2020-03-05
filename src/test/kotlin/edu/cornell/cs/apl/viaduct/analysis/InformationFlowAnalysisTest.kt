8416f70c0d203696562ff265cf1ba3164e5fa3a5
Cosku Acay <coskuacay@gmail.com>
2020-03-05 16:58:19 -0500

    Add test for constraint graph ouput in DOT format

--- src/test/kotlin/edu/cornell/cs/apl/viaduct/analysis/InformationFlowAnalysisTest.kt
+++ src/test/kotlin/edu/cornell/cs/apl/viaduct/analysis/InformationFlowAnalysisTest.kt
@@ -4,6 +4,7 @@ import edu.cornell.cs.apl.attributes.Tree
 import edu.cornell.cs.apl.viaduct.ExampleProgramProvider
 import edu.cornell.cs.apl.viaduct.passes.elaborated
 import edu.cornell.cs.apl.viaduct.syntax.surface.ProgramNode
+import java.io.StringWriter
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
 
@@ -13,5 +14,14 @@ internal class InformationFlowAnalysisTest {
     fun `it information flow checks`(program: ProgramNode) {
         val nameAnalysis = NameAnalysis(Tree(program.elaborated()))
         InformationFlowAnalysis(nameAnalysis).check()
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(ExampleProgramProvider::class)
+    fun `it has a valid constraint graph representation`(program: ProgramNode) {
+        val nameAnalysis = NameAnalysis(Tree(program.elaborated()))
+        val informationFlowAnalysis = InformationFlowAnalysis(nameAnalysis)
+        val writer = StringWriter()
+        informationFlowAnalysis.exportConstraintGraph(writer)
     }
 }

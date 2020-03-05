e67fbbca8d82894bf58bc6e7ef19814cdb10824a
Cosku Acay <coskuacay@gmail.com>
2020-03-05 13:40:52 -0500

    Add helpers for accessing all declarations in the program

---
+++ src/main/kotlin/edu/cornell/cs/apl/viaduct/analysis/Declarations.kt
@@ -0,0 +1,48 @@
+package edu.cornell.cs.apl.viaduct.analysis
+
+import edu.cornell.cs.apl.viaduct.protocols.MainProtocol
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.DeclarationNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.LetNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.Node
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.ProcessDeclarationNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.ProgramNode
+
+/** Returns all [LetNode]s in this node. */
+fun Node.letNodes(): List<LetNode> {
+    val result = mutableListOf<LetNode>()
+    fun traverse(node: Node) {
+        if (node is LetNode) {
+            result.add(node)
+        }
+        node.children.forEach(::traverse)
+    }
+    traverse(this)
+    return result
+}
+
+/** Returns all [DeclarationNode]s in this node. */
+fun Node.declarationNodes(): List<DeclarationNode> {
+    val result = mutableListOf<DeclarationNode>()
+    fun traverse(node: Node) {
+        if (node is DeclarationNode) {
+            result.add(node)
+        }
+        node.children.forEach(::traverse)
+    }
+    traverse(this)
+    return result
+}
+
+/**
+ * Returns the declaration of the [MainProtocol] in this program.
+ *
+ * TODO: throws blah blah
+ */
+fun ProgramNode.main(): ProcessDeclarationNode {
+    this.forEach {
+        if (it is ProcessDeclarationNode && it.protocol.value == MainProtocol)
+            return it
+    }
+    // TODO: custom error message
+    error("No main")
+}
--- src/test/kotlin/edu/cornell/cs/apl/viaduct/analysis/NameAnalysisTest.kt
+++ src/test/kotlin/edu/cornell/cs/apl/viaduct/analysis/NameAnalysisTest.kt
@@ -3,8 +3,6 @@ package edu.cornell.cs.apl.viaduct.analysis
 import edu.cornell.cs.apl.attributes.Tree
 import edu.cornell.cs.apl.viaduct.ExampleProgramProvider
 import edu.cornell.cs.apl.viaduct.passes.elaborated
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.LetNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.Node
 import edu.cornell.cs.apl.viaduct.syntax.surface.ProgramNode
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
@@ -21,21 +19,6 @@ internal class NameAnalysisTest {
     fun `temporary definitions are mapped to reads`(program: ProgramNode) {
         val elaborated = program.elaborated()
         val nameAnalysis = NameAnalysis(Tree(elaborated))
-        temporaryDefinitions(elaborated).forEach { definition ->
-            nameAnalysis.readers(definition)
-        }
+        elaborated.letNodes().forEach { declaration -> nameAnalysis.readers(declaration) }
     }
-}
-
-/** Returns the set of all temporary definitions in the program. */
-private fun temporaryDefinitions(program: edu.cornell.cs.apl.viaduct.syntax.intermediate.ProgramNode): Set<LetNode> {
-    val definitions: MutableSet<LetNode> = mutableSetOf()
-    fun traverse(node: Node) {
-        if (node is LetNode) {
-            definitions.add(node)
-        }
-        node.children.forEach(::traverse)
-    }
-    traverse(program)
-    return definitions
 }

d0e58b298d906cf39b9e53415abf4049b5627ce4
Cosku Acay <coskuacay@gmail.com>
2020-03-17 13:08:29 -0400

    Type check after splitting in tests

--- src/test/kotlin/edu/cornell/cs/apl/viaduct/passes/SplittingTest.kt
+++ src/test/kotlin/edu/cornell/cs/apl/viaduct/passes/SplittingTest.kt
@@ -7,6 +7,8 @@ import edu.cornell.cs.apl.viaduct.analysis.NameAnalysis
 import edu.cornell.cs.apl.viaduct.analysis.ProtocolAnalysis
 import edu.cornell.cs.apl.viaduct.analysis.TypeAnalysis
 import edu.cornell.cs.apl.viaduct.analysis.main
+import edu.cornell.cs.apl.viaduct.protocols.HostInterface
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.ProcessDeclarationNode
 import edu.cornell.cs.apl.viaduct.syntax.surface.ProgramNode
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ArgumentsSource
@@ -23,6 +25,11 @@ internal class SplittingTest {
             nameAnalysis.tree.root.main.selectProtocols(nameAnalysis, informationFlowAnalysis)
         val protocolAnalysis = ProtocolAnalysis(nameAnalysis, dumpProtocolAssignment)
 
-        nameAnalysis.tree.root.splitMain(protocolAnalysis, typeAnalysis)
+        val splitProgram = nameAnalysis.tree.root.splitMain(protocolAnalysis, typeAnalysis)
+        edu.cornell.cs.apl.viaduct.syntax.intermediate.ProgramNode(
+            // TODO: don't remove [HostInterface]s once [splitMain] starts renaming communication with main.
+            declarations = splitProgram.filterNot { it is ProcessDeclarationNode && it.protocol.value is HostInterface },
+            sourceLocation = splitProgram.sourceLocation
+        ).check()
     }
 }

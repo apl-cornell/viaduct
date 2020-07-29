53c7d51e6d52fb18fbf328f1cf45446a1463b9d2
cacay <coskuacay@gmail.com>
2020-07-29 16:28:02 -0400

    Add bimaps as a dependency

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -28,9 +28,10 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.8")
 
-    // Functional data structures
+    // Data structures
     implementation("io.vavr:vavr:1.0.0-alpha-3")
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3")
+    implementation("com.uchuhimo:kotlinx-bimap:1.2")
 
     // Google's Guava (core data structures)
     implementation("com.google.guava:guava:29.0-jre")

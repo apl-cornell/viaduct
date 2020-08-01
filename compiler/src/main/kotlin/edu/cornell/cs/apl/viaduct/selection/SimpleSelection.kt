dc1c31c5103b36a7d68484b965b8323b112707b2
Cosku Acay <coskuacay@gmail.com>
2020-07-31 21:06:16 -0400

    Fix inappropriate use of memoization in SimpleSelection

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/selection/SimpleSelection.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/selection/SimpleSelection.kt
@@ -1,9 +1,7 @@
 package edu.cornell.cs.apl.viaduct.selection
 
-import edu.cornell.cs.apl.attributes.attribute
 import edu.cornell.cs.apl.viaduct.analysis.InformationFlowAnalysis
 import edu.cornell.cs.apl.viaduct.analysis.NameAnalysis
-import edu.cornell.cs.apl.viaduct.analysis.tree
 import edu.cornell.cs.apl.viaduct.protocols.Local
 import edu.cornell.cs.apl.viaduct.syntax.HostTrustConfiguration
 import edu.cornell.cs.apl.viaduct.syntax.Protocol
@@ -18,53 +16,49 @@ import edu.cornell.cs.apl.viaduct.syntax.intermediate.QueryNode
 import kotlinx.collections.immutable.PersistentMap
 import kotlinx.collections.immutable.persistentMapOf
 
-/** This class implements a particularly simple but ineffective protocol selection.
-Along with a protocol selector, it takes as input a function [protocolCost] which
-gives a total linear order on protocol cost.
+private typealias PartialAssignment = PersistentMap<Variable, Protocol>
 
- **/
-class SimpleSelection(val program: ProgramNode, val selector: ProtocolSelector, val protocolCost: (Protocol) -> Int) {
-    private val tree = program.tree
+/**
+ * This class implements a particularly simple but ineffective protocol selection.
+ * Along with a protocol selector, it takes as input a function [protocolCost] which
+ * gives a total linear order on protocol cost.
+ */
+class SimpleSelection(
+    program: ProgramNode,
+    private val selector: ProtocolSelector,
+    private val protocolCost: (Protocol) -> Int
+) {
     private val nameAnalysis = NameAnalysis.get(program)
     private val informationFlowAnalysis = InformationFlowAnalysis.get(program)
-    val hostTrustConfiguration = HostTrustConfiguration(program)
+    private val hostTrustConfiguration = HostTrustConfiguration(program)
+
+    private fun possibleProtocols(node: LetNode, assignment: PartialAssignment): Set<Protocol> =
+        when (val value = node.value) {
+            is InputNode ->
+                setOf(Local(value.host.value))
+            is QueryNode ->
+                possibleProtocols(nameAnalysis.declaration(value), assignment)
+            else ->
+                selector.select(node, assignment)
+        }
+
+    private fun possibleProtocols(node: DeclarationNode, assignment: PartialAssignment): Set<Protocol> =
+        selector.select(node, assignment)
 
     fun select(processDeclaration: ProcessDeclarationNode): (Variable) -> Protocol {
         var assignment: PersistentMap<Variable, Protocol> = persistentMapOf()
-
-        val protocolSelection = object {
-            private val LetNode.possibleProtocols: Set<Protocol> by attribute {
-                when (value) {
-                    is InputNode ->
-                        setOf(Local(value.host.value))
-                    is QueryNode -> nameAnalysis.declaration(value).possibleProtocols
-                    else ->
-                        selector.select(this, assignment)
-                }
-            }
-
-            private val DeclarationNode.possibleProtocols: Set<Protocol> by attribute {
-                selector.select(this, assignment)
-            }
-
-            fun possibleProtocols(node: LetNode) = node.possibleProtocols
-
-            fun possibleProtocols(node: DeclarationNode) = node.possibleProtocols
-        }
 
         fun traverse(node: Node) {
             when (node) {
                 is LetNode -> {
                     // TODO: proper error class
-                    val p = protocolSelection.possibleProtocols(node).sortedBy(protocolCost)
-                        .firstOrNull() ?: error("protocol not found!")
+                    val p = possibleProtocols(node, assignment).minBy(protocolCost) ?: error("protocol not found!")
                     assert(p.authority(hostTrustConfiguration).actsFor(informationFlowAnalysis.label(node)))
                     assignment = assignment.put(node.temporary.value, p)
                 }
                 is DeclarationNode -> {
                     // TODO: proper error class
-                    val p = protocolSelection.possibleProtocols(node).sortedBy(protocolCost)
-                        .firstOrNull() ?: error("protocol not found!")
+                    val p = possibleProtocols(node, assignment).minBy(protocolCost) ?: error("protocol not found!")
                     assert(p.authority(hostTrustConfiguration).actsFor(informationFlowAnalysis.label(node)))
                     assignment = assignment.put(node.variable.value, p)
                 }

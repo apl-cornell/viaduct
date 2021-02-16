6fb7ce6f9eb9e6b21b8d313fe4a5e26e0545fdf8
Cosku Acay <coskuacay@gmail.com>
2021-02-16 15:43:27 -0500

    Bump JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,9 +81,9 @@ subprojects {
             "testImplementation"("org.apache.logging.log4j:log4j-slf4j-impl:2.14.0")
 
             // Testing
-            "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.0")
-            "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.7.0")
-            "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+            "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.7.1")
+            "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.7.1")
+            "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.7.1")
         }
 
         /** Testing */

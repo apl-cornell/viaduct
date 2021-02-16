8ba6854b94e9a570d9fb19bc22917d9c8fa6f4df
Cosku Acay <coskuacay@gmail.com>
2021-02-16 15:45:49 -0500

    Bump Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.4.20" apply false
+    kotlin("multiplatform") version "1.4.30" apply false
     id("org.jetbrains.dokka") version "1.4.20"
 
     // Style checking

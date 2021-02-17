7ca5d95b4ab5fb48d8bfc9089cd4d34a7f639f2d
Cosku Acay <coskuacay@gmail.com>
2021-02-17 16:16:52 -0500

    Track JUnit version in the test-utilities module

--- test-utilities/build.gradle.kts
+++ test-utilities/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 dependencies {
     implementation(project(":compiler"))
 
-    implementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
-    implementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
+    api("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    api("org.junit.jupiter:junit-jupiter-params:5.7.1")
+    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.1")
 }

aad43cf08a956c30bf90adbf1477964aac8d38ef
Cosku Acay <coskuacay@gmail.com>
2021-02-19 13:26:40 -0500

    Move the repositories declaration into settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,3 +4,12 @@ include("cli")
 include("runtime")
 include("shared")
 include("test-utilities")
+
+dependencyResolutionManagement {
+    repositories {
+        jcenter()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}

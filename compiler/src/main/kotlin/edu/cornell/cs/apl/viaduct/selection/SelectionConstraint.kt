f5d2c3308884a04bf9ddc3600d7afc92ebfe0b86
Cosku Acay <coskuacay@gmail.com>
2021-04-11 20:22:48 -0400

    Adapt to new Z3 interface

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/selection/SelectionConstraint.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/selection/SelectionConstraint.kt
@@ -4,6 +4,7 @@ import com.microsoft.z3.ArithExpr
 import com.microsoft.z3.BoolExpr
 import com.microsoft.z3.Context
 import com.microsoft.z3.IntExpr
+import com.microsoft.z3.IntSort
 import com.uchuhimo.collections.BiMap
 import edu.cornell.cs.apl.prettyprinting.Document
 import edu.cornell.cs.apl.prettyprinting.PrettyPrintable
@@ -228,7 +229,7 @@ internal fun SymbolicCost.arithExpr(
     ctx: Context,
     vmap: BiMap<FunctionVariable, IntExpr>,
     pmap: BiMap<Protocol, IntExpr>
-): ArithExpr =
+): ArithExpr<IntSort> =
     when (this) {
         is CostLiteral -> ctx.mkInt(this.cost)
         is CostVariable -> this.variable

994a719ad7a173f0e8fa4c910d76c3daf0587728
Ian Wilkie Tomasik <iw68@cornell.edu>
2021-07-29 15:16:20 -0400

    implemented bare bones AbstractCodeGeneration class

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/codegeneration/AbstractCodeGenerator.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/codegeneration/AbstractCodeGenerator.kt
@@ -1,25 +1,46 @@
 package edu.cornell.cs.apl.viaduct.codegeneration
 
-import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.FunSpec
-import com.squareup.kotlinpoet.MemberName.Companion.member
-import com.squareup.kotlinpoet.asClassName
+import com.squareup.kotlinpoet.CodeBlock
+import edu.cornell.cs.apl.viaduct.errors.IllegalInternalCommunicationError
+import edu.cornell.cs.apl.viaduct.syntax.Protocol
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.DeclarationNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.LetNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.OutParameterInitializationNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.OutputNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.ProgramNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.SendNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.SimpleStatementNode
+import edu.cornell.cs.apl.viaduct.syntax.intermediate.UpdateNode
 
-abstract class AbstractCodeGenerator {
-    companion object {
-        val test: String
-            get() {
-                val aby = de.tu_darmstadt.cs.encrypto.aby.Aby::class.asClassName()
-                val createNewShare = aby.member("createNewName")
-                return FileSpec.builder("", "Test")
-                    .addFunction(
-                        FunSpec.builder("test")
-                            .returns(de.tu_darmstadt.cs.encrypto.aby.Share::class)
-                            .addStatement("%M()", createNewShare)
-                            .build()
-                    )
-                    .build()
-                    .toString()
-            }
+abstract class AbstractCodeGenerator(
+    val program: ProgramNode
+) : CodeGenerator {
+
+    override fun genSimpleStatement(protocol: Protocol, stmt: SimpleStatementNode): CodeBlock {
+        when (stmt) {
+            is LetNode -> genLet(protocol, stmt)
+
+            is DeclarationNode -> genDeclaration(protocol, stmt)
+
+            is UpdateNode -> genUpdate(protocol, stmt)
+
+            is OutParameterInitializationNode -> genOutParameter(protocol, stmt)
+
+            is OutputNode -> genOutput(protocol, stmt)
+
+            is SendNode -> throw IllegalInternalCommunicationError(stmt)
+        }
+
+        throw IllegalArgumentException("unknown statement type")
     }
+
+    abstract fun genLet(protocol: Protocol, stmt: LetNode): CodeBlock
+
+    abstract fun genDeclaration(protocol: Protocol, stmt: DeclarationNode): CodeBlock
+
+    abstract fun genUpdate(protocol: Protocol, stmt: UpdateNode): CodeBlock
+
+    abstract fun genOutParameter(protocol: Protocol, stmt: OutParameterInitializationNode): CodeBlock
+
+    abstract fun genOutput(protocol: Protocol, stmt: OutputNode): CodeBlock
 }

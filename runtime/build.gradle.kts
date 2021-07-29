909ddd725f81435920e1f6b5394f85d177ec7b57
Cosku Acay <coskuacay@gmail.com>
2021-07-29 15:45:43 -0400

    Pull aby-java from GitHub Package Repository

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
 
     // Cryptography
-    implementation("com.github.apl-cornell:aby-java:9f626e2b70")
+    implementation("de.tu_darmstadt.cs.encrypto:aby-java:f96aceac2c7096499c26d91cad1e560e069f6aa2")
     implementation(files("libs/jsnark.jar"))
 
     // Testing

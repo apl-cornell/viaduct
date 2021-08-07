176bfc6854db8e4e56fffdd80ed0b4698b3d9203
Ian Wilkie Tomasik <iw68@cornell.edu>
2021-08-06 23:15:10 -0400

    minor change to CodeGenerator interface

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/codegeneration/CodeGenerator.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/codegeneration/CodeGenerator.kt
@@ -2,6 +2,7 @@ package edu.cornell.cs.apl.viaduct.codegeneration
 
 import com.squareup.kotlinpoet.CodeBlock
 import edu.cornell.cs.apl.viaduct.selection.ProtocolCommunication
+import edu.cornell.cs.apl.viaduct.syntax.Host
 import edu.cornell.cs.apl.viaduct.syntax.Protocol
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.AtomicExpressionNode
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.LetNode
@@ -15,17 +16,17 @@ interface CodeGenerator {
     fun SimpleStatement(protocol: Protocol, stmt: SimpleStatementNode): CodeBlock
 
     fun Send(
+        sendingHost: Host,
         sender: LetNode,
         sendProtocol: Protocol,
-        receiver: SimpleStatementNode,
         recvProtocol: Protocol,
         events: ProtocolCommunication
     ): CodeBlock
 
     fun Recieve(
+        receivingHost: Host,
         sender: LetNode,
         sendProtocol: Protocol,
-        receiver: SimpleStatementNode,
         recvProtocol: Protocol,
         events: ProtocolCommunication
     ): CodeBlock

3310fcd134938da6680d99ddbe97a4bc17cab2b4
Ian Wilkie Tomasik <iw68@cornell.edu>
2021-08-06 23:13:12 -0400

    created runtime interface for code generation

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/codegeneration/Runtime.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/codegeneration/Runtime.kt
@@ -1,4 +1,16 @@
 package edu.cornell.cs.apl.viaduct.codegeneration
 
+import edu.cornell.cs.apl.viaduct.syntax.Host
+import edu.cornell.cs.apl.viaduct.syntax.types.ValueType
+import edu.cornell.cs.apl.viaduct.syntax.values.Value
+import java.io.Serializable
+
 interface Runtime {
+    suspend fun input(type: ValueType): Value
+
+    suspend fun output(value: Value)
+
+    suspend fun send(value: Serializable, receiver: Host)
+
+    suspend fun receive(sender: Host): Serializable
 }

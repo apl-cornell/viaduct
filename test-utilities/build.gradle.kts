ab993b68262cf80a1c4cdad0e5246c6c71d29f4c
Cosku Acay <coskuacay@gmail.com>
2021-08-26 22:02:59 -0400

    Specify version for only one JUnit dependency

--- test-utilities/build.gradle.kts
+++ test-utilities/build.gradle.kts
@@ -8,6 +8,6 @@ dependencies {
     implementation(project(":compiler"))
 
     api("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    api("org.junit.jupiter:junit-jupiter-params:5.7.2")
-    runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    api("org.junit.jupiter:junit-jupiter-params")
+    runtimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }

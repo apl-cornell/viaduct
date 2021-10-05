71c3964b308dffc49b57bf4f6e05d108e4076ae2
Cosku Acay <coskuacay@gmail.com>
2021-10-05 19:19:56 -0400

    Remove ABY dependency from runtime2

--- runtime2/build.gradle.kts
+++ runtime2/build.gradle.kts
@@ -11,7 +11,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.0")
 
     // Cryptography
-    implementation("de.tu_darmstadt.cs.encrypto:aby-java:f96aceac2c7096499c26d91cad1e560e069f6aa2")
 
     // Testing
     testImplementation(project(":test-utilities"))

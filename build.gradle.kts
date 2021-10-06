7f1e3167e5d1ac21b9f96f24eb2685a0392a1ba8
Cosku Acay <coskuacay@gmail.com>
2021-10-06 00:27:44 -0400

    Use version of Kotlin embedded in Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.5.21" apply false
+    kotlin("multiplatform") version embeddedKotlinVersion apply false
 
     // Documentation
     id("org.jetbrains.dokka") version "1.5.31"

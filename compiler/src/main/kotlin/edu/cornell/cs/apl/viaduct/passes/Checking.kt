1b7b7f907d0a1b3e0f2cdb269017c83404bebc93
Cosku Acay <coskuacay@gmail.com>
2021-11-01 19:02:53 -0400

    Improve logging for analyses

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/passes/Checking.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/passes/Checking.kt
@@ -6,8 +6,8 @@ import edu.cornell.cs.apl.viaduct.analysis.OutParameterInitializationAnalysis
 import edu.cornell.cs.apl.viaduct.analysis.TypeAnalysis
 import edu.cornell.cs.apl.viaduct.errors.CompilationError
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.ProgramNode
+import edu.cornell.cs.apl.viaduct.util.duration
 import mu.KotlinLogging
-import kotlin.system.measureTimeMillis
 
 private val logger = KotlinLogging.logger("Check")
 
@@ -17,18 +17,11 @@ private val logger = KotlinLogging.logger("Check")
  * @throws CompilationError if there are errors in the program.
  */
 fun ProgramNode.check() {
-    logger.info { "name analysis..." }
-    NameAnalysis.get(this).check()
+    logger.duration("name analysis") { NameAnalysis.get(this).check() }
 
-    logger.info { "type checking..." }
-    TypeAnalysis.get(this).check()
+    logger.duration("type analysis") { TypeAnalysis.get(this).check() }
 
-    logger.info { "out parameter initialization analysis..." }
-    OutParameterInitializationAnalysis.get(this).check()
+    logger.duration("out parameter initialization analysis") { OutParameterInitializationAnalysis.get(this).check() }
 
-    val duration = measureTimeMillis {
-        logger.info { "information flow analysis..." }
-        InformationFlowAnalysis.get(this).check()
-    }
-    logger.info { "finished information flow analysis, ran for ${duration}ms" }
+    logger.duration("information flow analysis") { InformationFlowAnalysis.get(this).check() }
 }

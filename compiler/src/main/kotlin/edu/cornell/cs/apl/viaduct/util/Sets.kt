5527817711c2a8bd9b4d906955927e53cdcf8ff3
Cosku Acay <coskuacay@gmail.com>
2021-11-01 22:27:37 -0400

    Add ProtocolParsers to the Backend interface

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/util/Sets.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/util/Sets.kt
@@ -1,6 +1,5 @@
 package edu.cornell.cs.apl.viaduct.util
 
 /** Returns the union of all given sets. */
-fun <T> Iterable<Set<T>>.unions(): Set<T> {
-    return this.fold(setOf()) { acc, s -> acc.union(s) }
-}
+fun <T> Iterable<Set<T>>.unions(): Set<T> =
+    this.fold(setOf()) { acc, s -> acc.union(s) }

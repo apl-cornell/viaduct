65f5f33d4bd78aca1efd5581d09b2588daac2645
Cosku Acay <coskuacay@gmail.com>
2021-11-01 22:27:37 -0400

    Add ProtocolParsers to the Backend interface

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/zkp/ZKPBackend.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/zkp/ZKPBackend.kt
@@ -3,14 +3,19 @@ package edu.cornell.cs.apl.viaduct.backends.zkp
 import edu.cornell.cs.apl.viaduct.backends.Backend
 import edu.cornell.cs.apl.viaduct.codegeneration.CodeGenerator
 import edu.cornell.cs.apl.viaduct.codegeneration.CodeGeneratorContext
+import edu.cornell.cs.apl.viaduct.parsing.ProtocolParser
 import edu.cornell.cs.apl.viaduct.selection.ProtocolComposer
 import edu.cornell.cs.apl.viaduct.selection.ProtocolFactory
+import edu.cornell.cs.apl.viaduct.syntax.Protocol
 import edu.cornell.cs.apl.viaduct.syntax.ProtocolName
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.ProgramNode
 
 object ZKPBackend : Backend {
     override val protocols: Set<ProtocolName>
         get() = setOf(ZKP.protocolName)
+
+    override val protocolParsers: Map<ProtocolName, ProtocolParser<Protocol>>
+        get() = mapOf(ZKP.protocolName to ZKPProtocolParser)
 
     override fun protocolFactory(program: ProgramNode): ProtocolFactory = ZKPProtocolFactory(program)
 

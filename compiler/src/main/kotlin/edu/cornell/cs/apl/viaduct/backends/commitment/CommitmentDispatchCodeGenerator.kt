f62a357c5607b2613011e7ee908b3f2419d1ed8f
Cosku Acay <coskuacay@gmail.com>
2021-11-21 19:12:53 -0500

    Simplify CommitmentDispatchCodeGenerator

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/commitment/CommitmentDispatchCodeGenerator.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/commitment/CommitmentDispatchCodeGenerator.kt
@@ -3,7 +3,6 @@ package edu.cornell.cs.apl.viaduct.backends.commitment
 import com.squareup.kotlinpoet.CodeBlock
 import edu.cornell.cs.apl.viaduct.codegeneration.CodeGenerator
 import edu.cornell.cs.apl.viaduct.codegeneration.CodeGeneratorContext
-import edu.cornell.cs.apl.viaduct.errors.CodeGenerationError
 import edu.cornell.cs.apl.viaduct.selection.ProtocolCommunication
 import edu.cornell.cs.apl.viaduct.syntax.Protocol
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.AtomicExpressionNode
@@ -16,29 +15,19 @@ class CommitmentDispatchCodeGenerator(
     private val commitmentCreatorGenerator = CommitmentCreatorGenerator(context)
     private val commitmentHolderGenerator = CommitmentHolderGenerator(context)
 
+    private fun generatorFor(protocol: Protocol): CodeGenerator {
+        require(protocol is Commitment)
+        return if (context.host == protocol.cleartextHost)
+            commitmentCreatorGenerator
+        else
+            commitmentHolderGenerator
+    }
+
     override fun guard(protocol: Protocol, expr: AtomicExpressionNode): CodeBlock =
-        when (protocol) {
-            is Commitment -> {
-                if (context.host == protocol.cleartextHost) {
-                    commitmentCreatorGenerator.guard(protocol, expr)
-                } else {
-                    commitmentHolderGenerator.guard(protocol, expr)
-                }
-            }
-            else -> throw CodeGenerationError("Commitment generator dispatcher: got non-commitment protocol")
-        }
+        generatorFor(protocol).guard(protocol, expr)
 
     override fun simpleStatement(protocol: Protocol, stmt: SimpleStatementNode): CodeBlock =
-        when (protocol) {
-            is Commitment -> {
-                if (context.host == protocol.cleartextHost) {
-                    commitmentCreatorGenerator.simpleStatement(protocol, stmt)
-                } else {
-                    commitmentHolderGenerator.simpleStatement(protocol, stmt)
-                }
-            }
-            else -> throw CodeGenerationError("Commitment generator dispatcher: got non-commitment protocol")
-        }
+        generatorFor(protocol).simpleStatement(protocol, stmt)
 
     override fun send(
         sender: LetNode,
@@ -46,16 +35,7 @@ class CommitmentDispatchCodeGenerator(
         receiveProtocol: Protocol,
         events: ProtocolCommunication
     ): CodeBlock =
-        when (sendProtocol) {
-            is Commitment -> {
-                if (context.host == sendProtocol.cleartextHost) {
-                    commitmentCreatorGenerator.send(sender, sendProtocol, receiveProtocol, events)
-                } else {
-                    commitmentHolderGenerator.send(sender, sendProtocol, receiveProtocol, events)
-                }
-            }
-            else -> throw CodeGenerationError("Commitment generator dispatcher: got non-commitment protocol")
-        }
+        generatorFor(sendProtocol).send(sender, sendProtocol, receiveProtocol, events)
 
     override fun receive(
         sender: LetNode,
@@ -63,14 +43,5 @@ class CommitmentDispatchCodeGenerator(
         receiveProtocol: Protocol,
         events: ProtocolCommunication
     ): CodeBlock =
-        when (receiveProtocol) {
-            is Commitment -> {
-                if (context.host == receiveProtocol.cleartextHost) {
-                    commitmentCreatorGenerator.receive(sender, sendProtocol, receiveProtocol, events)
-                } else {
-                    commitmentHolderGenerator.receive(sender, sendProtocol, receiveProtocol, events)
-                }
-            }
-            else -> throw CodeGenerationError("Commitment generator dispatcher: got non-commitment protocol")
-        }
+        generatorFor(receiveProtocol).receive(sender, sendProtocol, receiveProtocol, events)
 }

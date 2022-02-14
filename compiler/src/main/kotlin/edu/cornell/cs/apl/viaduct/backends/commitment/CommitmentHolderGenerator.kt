f3ead9d4161b587f5d85acbfc5ee21d31f75913e
Josh Acay <coskuacay@gmail.com>
2022-02-14 18:26:21 -0500

    Move query and update compilation to AbstractCodeGenerator (#350)

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/commitment/CommitmentHolderGenerator.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/commitment/CommitmentHolderGenerator.kt
@@ -4,7 +4,6 @@ import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.asClassName
-import edu.cornell.cs.apl.viaduct.analysis.NameAnalysis
 import edu.cornell.cs.apl.viaduct.analysis.TypeAnalysis
 import edu.cornell.cs.apl.viaduct.codegeneration.AbstractCodeGenerator
 import edu.cornell.cs.apl.viaduct.codegeneration.CodeGeneratorContext
@@ -17,119 +16,32 @@ import edu.cornell.cs.apl.viaduct.selection.CommunicationEvent
 import edu.cornell.cs.apl.viaduct.selection.ProtocolCommunication
 import edu.cornell.cs.apl.viaduct.syntax.Protocol
 import edu.cornell.cs.apl.viaduct.syntax.ProtocolProjection
-import edu.cornell.cs.apl.viaduct.syntax.datatypes.Get
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.AtomicExpressionNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.DowngradeNode
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.ExpressionNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.InputNode
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.LetNode
 import edu.cornell.cs.apl.viaduct.syntax.intermediate.LiteralNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.OperatorApplicationNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.OutputNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.QueryNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.ReadNode
-import edu.cornell.cs.apl.viaduct.syntax.intermediate.UpdateNode
-import edu.cornell.cs.apl.viaduct.syntax.types.ImmutableCellType
-import edu.cornell.cs.apl.viaduct.syntax.types.MutableCellType
-import edu.cornell.cs.apl.viaduct.syntax.types.VectorType
 import edu.cornell.cs.apl.viaduct.backends.commitment.Commitment as CommitmentProtocol
 
 internal class CommitmentHolderGenerator(
     context: CodeGeneratorContext
 ) : AbstractCodeGenerator(context) {
     private val typeAnalysis: TypeAnalysis = TypeAnalysis.get(context.program)
-    private val nameAnalysis: NameAnalysis = NameAnalysis.get(context.program)
-    override fun exp(protocol: Protocol, expr: ExpressionNode): CodeBlock =
-        when (expr) {
-            is LiteralNode -> CodeBlock.of(
-                "%M(%L).%M()",
-                MemberName(Committed.Companion::class.asClassName(), "fake"),
-                expr.value,
-                MemberName(Committed.Companion::class.asClassName(), "commitment")
-            )
-
-            is ReadNode -> CodeBlock.of(
-                "%L",
-                context.kotlinName(expr.temporary.value, protocol)
-            )
-
-            is DowngradeNode -> exp(protocol, expr.expression)
-
-            is QueryNode ->
-                when (typeAnalysis.type(nameAnalysis.declaration(expr))) {
-                    is VectorType -> {
-                        when (expr.query.value) {
-                            is Get -> CodeBlock.of(
-                                "%N[%L]",
-                                context.kotlinName(expr.variable.value),
-                                cleartextExp(protocol, expr.arguments.first())
-                            )
-                            else -> throw UnsupportedOperatorException(protocol, expr)
-                        }
-                    }
-
-                    is ImmutableCellType -> {
-                        when (expr.query.value) {
-                            is Get -> CodeBlock.of(context.kotlinName(expr.variable.value))
-                            else -> throw UnsupportedOperatorException(protocol, expr)
-                        }
-                    }
-
-                    is MutableCellType -> {
-                        when (expr.query.value) {
-                            is Get -> CodeBlock.of(context.kotlinName(expr.variable.value))
-                            else -> throw UnsupportedOperatorException(protocol, expr)
-                        }
-                    }
-
-                    else -> throw UnsupportedOperatorException(protocol, expr)
-                }
-
-            is OperatorApplicationNode ->
-                throw UnsupportedOperatorException(protocol, expr)
-
-            is InputNode ->
-                throw UnsupportedOperatorException(protocol, expr)
-        }
-
-    override fun let(protocol: Protocol, stmt: LetNode): CodeBlock =
-        CodeBlock.of(
-            "var %N = %L",
-            context.kotlinName(stmt.temporary.value, protocol),
-            exp(protocol, stmt.value)
-        )
-
-    override fun update(protocol: Protocol, stmt: UpdateNode): CodeBlock =
-        when (typeAnalysis.type(nameAnalysis.declaration(stmt))) {
-            is VectorType ->
-                when (stmt.update.value) {
-                    is edu.cornell.cs.apl.viaduct.syntax.datatypes.Set ->
-                        CodeBlock.of(
-                            "%N[%L] = %L",
-                            context.kotlinName(stmt.variable.value),
-                            cleartextExp(protocol, stmt.arguments[0]),
-                            exp(protocol, stmt.arguments[1])
-                        )
-                    else -> throw UnsupportedOperatorException(protocol, stmt)
-                }
-            is MutableCellType ->
-                when (stmt.update.value) {
-                    is edu.cornell.cs.apl.viaduct.syntax.datatypes.Set ->
-                        CodeBlock.of(
-                            "%N = %L",
-                            context.kotlinName(stmt.variable.value),
-                            exp(protocol, stmt.arguments[0])
-                        )
-                    else -> throw UnsupportedOperatorException(protocol, stmt)
-                }
-            else -> throw UnsupportedOperatorException(protocol, stmt)
-        }
-
-    override fun output(protocol: Protocol, stmt: OutputNode): CodeBlock =
-        throw UnsupportedOperatorException(protocol, stmt)
 
     override fun guard(protocol: Protocol, expr: AtomicExpressionNode): CodeBlock =
         throw UnsupportedOperatorException(protocol, expr)
+
+    override fun exp(protocol: Protocol, expr: ExpressionNode): CodeBlock =
+        when (expr) {
+            is LiteralNode -> CodeBlock.of(
+                "%T.%N(%L).%M()",
+                Committed::class,
+                "fake",
+                value(expr.value),
+                MemberName(Committed.Companion::class.asClassName(), "commitment")
+            )
+
+            else -> super.exp(protocol, expr)
+        }
 
     override fun send(
         sender: LetNode,
@@ -151,7 +63,7 @@ internal class CommitmentHolderGenerator(
                 sendBuilder.addStatement(
                     "%L",
                     context.send(
-                        CodeBlock.of("%L", context.kotlinName(sender.temporary.value, sendProtocol)),
+                        CodeBlock.of("%N", context.kotlinName(sender.temporary.value, sendProtocol)),
                         event.recv.host
                     )
                 )

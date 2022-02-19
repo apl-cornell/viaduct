a8819e2b162a35c59f0b7062300ca26e0d60a89a
Cosku Acay <coskuacay@gmail.com>
2022-02-18 22:35:53 -0500

    Delete unused error class

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/errors/ImpossibleCaseError.kt
+++
@@ -1,6 +0,0 @@
-package edu.cornell.cs.apl.viaduct.errors
-
-/** Thrown when the compiler reaches an impossible state. */
-abstract class ImpossibleCaseError : CompilationError() {
-    override val category = "Impossible Case"
-}

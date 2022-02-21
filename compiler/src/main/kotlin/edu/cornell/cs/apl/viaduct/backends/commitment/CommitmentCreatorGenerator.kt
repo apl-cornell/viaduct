f267abf6ef54070de37ab342bb3b2ad312ac7faa
Ian Wilkie Tomasik <ianwtmjj98@gmail.com>
2022-02-20 21:33:56 -0500

    incorporate feedback

--- compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/commitment/CommitmentCreatorGenerator.kt
+++ compiler/src/main/kotlin/edu/cornell/cs/apl/viaduct/backends/commitment/CommitmentCreatorGenerator.kt
@@ -53,7 +53,7 @@ internal class CommitmentCreatorGenerator(
                 sendBuilder.addStatement(
                     "%L",
                     context.send(
-                        CodeBlock.of("%L", context.kotlinName(sender.name.value, sendProtocol)),
+                        CodeBlock.of("%N", context.kotlinName(sender.name.value, sendProtocol)),
                         event.recv.host
                     )
                 )

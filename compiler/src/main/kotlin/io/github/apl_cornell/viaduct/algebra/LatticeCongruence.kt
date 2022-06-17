b8287152d038c8b72ead4266c09834b91289de03
s-ren <sr2262@cornell.edu>
2022-06-17 14:56:27 -0400

    format files

--- compiler/src/main/kotlin/io/github/apl_cornell/viaduct/algebra/LatticeCongruence.kt
+++ compiler/src/main/kotlin/io/github/apl_cornell/viaduct/algebra/LatticeCongruence.kt
@@ -11,7 +11,12 @@ class FreeDistributiveLatticeCongruence<A>(
     congruence: List<Congruence<FreeDistributiveLattice<A>>>
 ) : LatticeCongruence<FreeDistributiveLattice<A>> {
     private val foldedCongruence: Congruence<FreeDistributiveLattice<A>> =
-        congruence.fold(Pair(FreeDistributiveLattice.bounds<A>().bottom, FreeDistributiveLattice.bounds<A>().bottom))
+        congruence.fold(
+            Pair(
+                FreeDistributiveLattice.bounds<A>().bottom,
+                FreeDistributiveLattice.bounds<A>().bottom
+            )
+        )
         { acc, element ->
             Pair(acc.first.meet(element.first), acc.second.join(element.second))
         }
@@ -20,14 +25,20 @@ class FreeDistributiveLatticeCongruence<A>(
         (first.meet(foldedCongruence.first) == second.meet(foldedCongruence.first)) &&
             (first.join(foldedCongruence.second) == second.join(foldedCongruence.second))
 
-    override fun lessThanOrEqualTo(first: FreeDistributiveLattice<A>, second: FreeDistributiveLattice<A>): Boolean =
+    override fun lessThanOrEqualTo(
+        first: FreeDistributiveLattice<A>,
+        second: FreeDistributiveLattice<A>
+    ): Boolean =
         equals(first.join(second), second)
 
     /**
      * Return a new congruence that has the congruence relation of this and other.
      */
     operator fun plus(other: FreeDistributiveLatticeCongruence<A>): FreeDistributiveLatticeCongruence<A> =
-        FreeDistributiveLatticeCongruence(listOf(this.foldedCongruence) + listOf(other.foldedCongruence))
+        FreeDistributiveLatticeCongruence(
+            listOf(this.foldedCongruence)
+                + listOf(other.foldedCongruence)
+        )
 
     companion object {
         private val EMPTY: FreeDistributiveLatticeCongruence<*> =

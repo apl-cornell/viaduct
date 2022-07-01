36b0b221fdbbed5a35a4cb303a48699bff20fcc6
Josh Acay <coskuacay@gmail.com>
2022-07-01 19:59:15 -0400

    Simplify CommitmentDispatchCodeGenerator (#463)

--- compiler/src/main/kotlin/io/github/apl_cornell/viaduct/backends/commitment/CommitmentDispatchCodeGenerator.kt
+++ compiler/src/main/kotlin/io/github/apl_cornell/viaduct/backends/commitment/CommitmentDispatchCodeGenerator.kt
@@ -1,60 +1,21 @@
 package io.github.apl_cornell.viaduct.backends.commitment
 
-import com.squareup.kotlinpoet.CodeBlock
-import com.squareup.kotlinpoet.PropertySpec
 import io.github.apl_cornell.viaduct.codegeneration.CodeGenerator
 import io.github.apl_cornell.viaduct.codegeneration.CodeGeneratorContext
-import io.github.apl_cornell.viaduct.selection.ProtocolCommunication
-import io.github.apl_cornell.viaduct.syntax.Arguments
-import io.github.apl_cornell.viaduct.syntax.ObjectTypeNode
+import io.github.apl_cornell.viaduct.codegeneration.CodeGeneratorDispatcher
 import io.github.apl_cornell.viaduct.syntax.Protocol
-import io.github.apl_cornell.viaduct.syntax.intermediate.AtomicExpressionNode
-import io.github.apl_cornell.viaduct.syntax.intermediate.ExpressionNode
-import io.github.apl_cornell.viaduct.syntax.intermediate.LetNode
-import io.github.apl_cornell.viaduct.syntax.intermediate.UpdateNode
 
 class CommitmentDispatchCodeGenerator(
-    val context: CodeGeneratorContext
-) : CodeGenerator {
+    private val context: CodeGeneratorContext
+) : CodeGeneratorDispatcher() {
     private val commitmentCreatorGenerator = CommitmentCreatorGenerator(context)
     private val commitmentHolderGenerator = CommitmentHolderGenerator(context)
 
-    private fun generatorFor(protocol: Protocol): CodeGenerator {
+    override fun generatorFor(protocol: Protocol): CodeGenerator {
         require(protocol is Commitment)
         return if (context.host == protocol.cleartextHost)
             commitmentCreatorGenerator
         else
             commitmentHolderGenerator
     }
-
-    override fun guard(protocol: Protocol, expr: AtomicExpressionNode): CodeBlock =
-        generatorFor(protocol).guard(protocol, expr)
-
-    override fun exp(protocol: Protocol, expr: ExpressionNode): CodeBlock = generatorFor(protocol).exp(protocol, expr)
-
-    override fun constructorCall(
-        protocol: Protocol,
-        objectType: ObjectTypeNode,
-        arguments: Arguments<AtomicExpressionNode>
-    ): CodeBlock = generatorFor(protocol).constructorCall(protocol, objectType, arguments)
-
-    override fun update(protocol: Protocol, stmt: UpdateNode): CodeBlock = generatorFor(protocol).update(protocol, stmt)
-
-    override fun send(
-        sender: LetNode,
-        sendProtocol: Protocol,
-        receiveProtocol: Protocol,
-        events: ProtocolCommunication
-    ): CodeBlock =
-        generatorFor(sendProtocol).send(sender, sendProtocol, receiveProtocol, events)
-
-    override fun receive(
-        sender: LetNode,
-        sendProtocol: Protocol,
-        receiveProtocol: Protocol,
-        events: ProtocolCommunication
-    ): CodeBlock =
-        generatorFor(receiveProtocol).receive(sender, sendProtocol, receiveProtocol, events)
-
-    override fun setup(protocol: Protocol): Iterable<PropertySpec> = listOf()
 }

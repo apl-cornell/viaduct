4636e70bdcc5c1c25be10052bfc37b7810d5cdd2
Cosku Acay <coskuacay@gmail.com>
2022-07-06 17:00:38 -0400

    Make Gurobi the default solver (when it exists)

--- compiler/src/main/kotlin/io/github/apl_cornell/viaduct/selection/Solvers.kt
+++ compiler/src/main/kotlin/io/github/apl_cornell/viaduct/selection/Solvers.kt
@@ -1,19 +1,22 @@
 package io.github.apl_cornell.viaduct.selection
 
+private val gurobiSolver: SelectionProblemSolver? =
+    try {
+        val gurobiClass = Class.forName("${PackageName.javaClass.packageName}.GurobiSelectionProblemSolver")
+        val instance = gurobiClass.getDeclaredField("INSTANCE").get(null) as SelectionProblemSolver
+        instance
+    } catch (e: ClassNotFoundException) {
+        null
+    }
+
 val defaultSelectionProblemSolver: SelectionProblemSolver =
-    Z3SelectionProblemSolver
+    gurobiSolver ?: Z3SelectionProblemSolver
 
 /** Returns a list name-constructor pairs for all [SelectionProblemSolver] classes. */
 val selectionProblemSolvers: List<Pair<String, SelectionProblemSolver>> =
     listOfNotNull(
         Pair("z3", Z3SelectionProblemSolver),
-        try {
-            val gurobiClass = Class.forName("${PackageName.javaClass.packageName}.GurobiSelectionProblemSolver")
-            val instance = gurobiClass.getDeclaredField("INSTANCE").get(null) as SelectionProblemSolver
-            Pair("gurobi", instance)
-        } catch (e: ClassNotFoundException) {
-            null
-        }
+        gurobiSolver?.let { Pair("gurobi", it) }
     )
 
 private object PackageName

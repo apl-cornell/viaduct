b5a03fd228657f52550b7e158e8c381835efedc9
Cosku Acay <coskuacay@gmail.com>
2022-10-21 22:41:07 -0400

    Fix inferred label printing

--- compiler/src/main/kotlin/io/github/apl_cornell/viaduct/passes/Compilation.kt
+++ compiler/src/main/kotlin/io/github/apl_cornell/viaduct/passes/Compilation.kt
@@ -1,7 +1,6 @@
 package io.github.apl_cornell.viaduct.passes
 
 import com.squareup.kotlinpoet.FileSpec
-import io.github.apl_cornell.viaduct.algebra.FreeDistributiveLattice
 import io.github.apl_cornell.viaduct.analysis.InformationFlowAnalysis
 import io.github.apl_cornell.viaduct.analysis.descendantsIsInstance
 import io.github.apl_cornell.viaduct.backends.Backend
@@ -13,8 +12,6 @@ import io.github.apl_cornell.viaduct.parsing.parse
 import io.github.apl_cornell.viaduct.prettyprinting.Document
 import io.github.apl_cornell.viaduct.prettyprinting.PrettyPrintable
 import io.github.apl_cornell.viaduct.prettyprinting.plus
-import io.github.apl_cornell.viaduct.security.LabelComponent
-import io.github.apl_cornell.viaduct.security.SecurityLattice
 import io.github.apl_cornell.viaduct.selection.ProtocolSelection
 import io.github.apl_cornell.viaduct.selection.SelectionProblemSolver
 import io.github.apl_cornell.viaduct.selection.SimpleCostEstimator
@@ -52,25 +49,20 @@ fun SourceFile.compile(
             parsed.elaborated()
         }
 
+        // Dump label constraint graph.
+        saveLabelConstraintGraph?.invoke(InformationFlowAnalysis.get(elaborated)::exportConstraintGraph)
+
         // Perform static checks.
         elaborated.check()
 
-        logger.duration("function specialization") {
+        val specialized = logger.duration("function specialization") {
             elaborated.specialize()
         }
-    }
 
-    // Dump label constraint graph.
-    saveLabelConstraintGraph?.invoke(InformationFlowAnalysis.get(program)::exportConstraintGraph)
-    if (saveInferredLabels != null) {
-        val labelMetadata: Metadata =
-            (
-                program.descendantsIsInstance<LetNode>()
-                    .map { it to SecurityLattice.Bounds<LabelComponent>(FreeDistributiveLattice.bounds()).weakest } +
-                    program.descendantsIsInstance<DeclarationNode>()
-                        .map { it to SecurityLattice.Bounds<LabelComponent>(FreeDistributiveLattice.bounds()).weakest }
-                ).toMap()
-        saveInferredLabels.dumpProgramMetadata(program, labelMetadata)
+        // Check that function specialization is sensible.
+        specialized.check()
+
+        specialized
     }
 
     // Dump program annotated with inferred labels.
@@ -85,9 +77,6 @@ fun SourceFile.compile(
                 ).toMap()
         saveInferredLabels.dumpProgramMetadata(program, labelMetadata)
     }
-
-    // Check the monomorphized program
-    program.check()
 
     // Select protocols.
     val protocolFactory = backend.protocolFactory(program)

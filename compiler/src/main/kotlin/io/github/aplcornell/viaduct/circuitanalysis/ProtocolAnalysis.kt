7673e66fa566676df696659854921ac02b3c3498
Vivian Ding <vyd2@cornell.edu>
2023-02-13 00:42:06 -0500

    Delete useless files

--- compiler/src/main/kotlin/io/github/aplcornell/viaduct/circuitanalysis/ProtocolAnalysis.kt
+++
@@ -1,8 +0,0 @@
-package io.github.aplcornell.viaduct.circuitanalysis
-
-import kotlinx.collections.immutable.PersistentSet
-import kotlinx.collections.immutable.persistentHashSetOf
-
-/** Returns the union of all sets in this collection. */
-private fun <E> Iterable<PersistentSet<E>>.unions(): PersistentSet<E> =
-    this.fold(persistentHashSetOf()) { acc, set -> acc.addAll(set) }

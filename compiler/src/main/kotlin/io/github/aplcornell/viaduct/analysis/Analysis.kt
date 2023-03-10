36f5e710bef15efb28f2c222e708a77e5541fe8c
Cosku Acay <coskuacay@gmail.com>
2023-03-09 21:40:12 -0500

    Add comments to AnalysisProvider

--- compiler/src/main/kotlin/io/github/aplcornell/viaduct/analysis/Analysis.kt
+++ compiler/src/main/kotlin/io/github/aplcornell/viaduct/analysis/Analysis.kt
@@ -1,4 +1,14 @@
 package io.github.aplcornell.viaduct.analysis
 
-/** An analysis over [Node]. */
+/**
+ * A class that computes information about a [Node].
+ * The information should be completely determined given the [Node].
+ * That is, an [Analysis] should act like a pure function from [Node]
+ * to some metadata.
+ *
+ * It is common to need the same analysis at multiple places in a compiler.
+ * For example, type information might be useful for different passes.
+ * Recompute type information over and over again would be inefficient.
+ * @see AnalysisProvider for a way to cache [Analysis] instances.
+ */
 interface Analysis<Node>
--- compiler/src/main/kotlin/io/github/aplcornell/viaduct/analysis/AnalysisProvider.kt
+++ compiler/src/main/kotlin/io/github/aplcornell/viaduct/analysis/AnalysisProvider.kt
@@ -5,16 +5,27 @@ import io.github.aplcornell.viaduct.attributes.TreeNode
 import io.github.aplcornell.viaduct.attributes.attribute
 import mu.KotlinLogging
 import kotlin.reflect.KClass
+import kotlin.reflect.full.primaryConstructor
 
 private val logger = KotlinLogging.logger("AnalysisProvider")
 
+/**
+ * Constructs and caches [Analysis] instances for a [RootNode] so that
+ * the same instance can be retrieved at multiple places in the compiler.
+ * This avoids recomputing the same information over and over again.
+ *
+ * This class uses reflection to construct [Analysis] instances.
+ * Each [Analysis] class must have a primary constructor. The constructor
+ * may have parameters of type: [TreeNode], [RootNode], and subclasses of [Analysis].
+ * Sensible arguments will be passed to the constructor based on parameter types.
+ */
 class AnalysisProvider<Node : TreeNode<Node>, RootNode : Node>(rootNode: RootNode) {
     /** A lazily constructed [Tree] instance for the root node. */
     val tree: Tree<Node, RootNode> by lazy { Tree(rootNode) }
 
     val KClass<Analysis<RootNode>>.instance: Analysis<RootNode> by attribute {
         logger.debug("Constructing instance for $this.")
-        val constructor = this.constructors.first()
+        val constructor = this.primaryConstructor!!
         val arguments = constructor.parameters.map {
             when (it.type.classifier) {
                 rootNode::class -> {

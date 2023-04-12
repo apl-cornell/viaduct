fa501555e69adc5f5052c049924c0a3171747fc5
Cosku Acay <coskuacay@gmail.com>
2023-04-12 02:21:31 -0400

    Update related dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.8.20" apply false
-    kotlin("plugin.serialization") version "1.8.10" apply false
+    kotlin("plugin.serialization") version "1.8.20" apply false
 
     // Versioning
     id("com.palantir.git-version") version "2.0.0"

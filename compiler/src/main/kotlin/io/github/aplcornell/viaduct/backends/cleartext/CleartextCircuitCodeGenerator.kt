720e8c8b88166b3e8a00a85cc4644c581742ea26
Aditya Senthilnathan <aditsep99@gmail.com>
2024-08-31 15:57:16 -0400

    Refactored CommitmentCircuitCodeGenerator into CleartextCircuitCodeGenerator

--- compiler/src/main/kotlin/io/github/aplcornell/viaduct/backends/cleartext/CleartextCircuitCodeGenerator.kt
+++ compiler/src/main/kotlin/io/github/aplcornell/viaduct/backends/cleartext/CleartextCircuitCodeGenerator.kt
@@ -2,12 +2,20 @@ package io.github.aplcornell.viaduct.backends.cleartext
 
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.MemberName
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.STRING
+import com.squareup.kotlinpoet.TypeName
+import com.squareup.kotlinpoet.asClassName
+import com.squareup.kotlinpoet.asTypeName
 import io.github.aplcornell.viaduct.circuitcodegeneration.AbstractCodeGenerator
 import io.github.aplcornell.viaduct.circuitcodegeneration.Argument
 import io.github.aplcornell.viaduct.circuitcodegeneration.CodeGeneratorContext
 import io.github.aplcornell.viaduct.circuitcodegeneration.UnsupportedCommunicationException
+import io.github.aplcornell.viaduct.circuitcodegeneration.kotlinType
 import io.github.aplcornell.viaduct.circuitcodegeneration.receiveExpected
 import io.github.aplcornell.viaduct.circuitcodegeneration.receiveReplicated
+import io.github.aplcornell.viaduct.runtime.commitment.Commitment
+import io.github.aplcornell.viaduct.runtime.commitment.Committed
 import io.github.aplcornell.viaduct.syntax.BinaryOperator
 import io.github.aplcornell.viaduct.syntax.Host
 import io.github.aplcornell.viaduct.syntax.Protocol
@@ -15,8 +23,13 @@ import io.github.aplcornell.viaduct.syntax.UnaryOperator
 import io.github.aplcornell.viaduct.syntax.circuit.OperatorNode
 import io.github.aplcornell.viaduct.syntax.operators.Maximum
 import io.github.aplcornell.viaduct.syntax.operators.Minimum
+import io.github.aplcornell.viaduct.syntax.types.ValueType
+import io.github.aplcornell.viaduct.backends.commitment.Commitment as CommitmentProtocol
+import io.github.aplcornell.viaduct.circuitcodegeneration.typeTranslator
+
 
 class CleartextCircuitCodeGenerator(context: CodeGeneratorContext) : AbstractCodeGenerator(context) {
+
     override fun operatorApplication(protocol: Protocol, op: OperatorNode, arguments: List<CodeBlock>): CodeBlock =
         when (op.operator) {
             Minimum ->
@@ -112,6 +125,121 @@ class CleartextCircuitCodeGenerator(context: CodeGeneratorContext) : AbstractCod
         }
     }
 
+    private fun createCommitment(
+        source: Protocol,
+        target: Protocol,
+        argument: Argument,
+        builder: CodeBlock.Builder,
+    ): CodeBlock {
+        require(context.host in source.hosts + target.hosts)
+        require(source is Local && source.hosts.size == 1 && source.host in source.hosts)
+        require(target is CommitmentProtocol)
+        if (target.cleartextHost != source.host || target.cleartextHost in target.hashHosts) {
+            throw UnsupportedCommunicationException(source, target, argument.sourceLocation)
+        }
+        val argType = kotlinType(argument.type.shape, typeTranslator(argument.type.elementType.value))
+        val sendingHost = target.cleartextHost
+        val receivingHosts = target.hashHosts
+        return when (context.host) {
+            sendingHost -> {
+                val tempName1 = context.newTemporary("CommitTemp")
+                val tempName2 = context.newTemporary("CommitTemp")
+                builder.addStatement(
+                    "val %N = %T(%L)",
+                    tempName1,
+                    (Committed::class).asTypeName().parameterizedBy(argType),
+                    argument.value,
+                )
+                builder.addStatement(
+                    "val %N = %N.%M()",
+                    tempName2,
+                    tempName1,
+                    MemberName(Committed.Companion::class.asClassName(), "commitment"),
+                )
+                receivingHosts.forEach {
+                    builder.addStatement("%L", context.send(CodeBlock.of("%N", tempName2), it))
+                }
+                CodeBlock.of("%N", tempName1)
+            }
+
+            in receivingHosts -> {
+                val tempName3 = context.newTemporary("CommitTemp")
+                builder.addStatement(
+                    "val %N = %L",
+                    tempName3,
+                    context.receive((Commitment::class).asTypeName().parameterizedBy(argType), source.host),
+                )
+                CodeBlock.of("%N", tempName3)
+            }
+
+            else -> throw IllegalStateException()
+        }
+    }
+
+    private fun openCommitment(
+        source: Protocol,
+        target: Protocol,
+        argument: Argument,
+        builder: CodeBlock.Builder,
+    ): CodeBlock {
+        require(source is CommitmentProtocol)
+        require(target is Replication)
+        require(context.host in source.hosts + target.hosts)
+        if (source.hashHosts != target.hosts || source.cleartextHost in source.hashHosts) {
+            throw UnsupportedCommunicationException(source, target, argument.sourceLocation)
+        }
+        val argType = kotlinType(argument.type.shape, typeTranslator(argument.type.elementType.value))
+        val sendingHost = source.cleartextHost
+        val receivingHosts = target.hosts
+        return when (context.host) {
+            sendingHost -> {
+                receivingHosts.forEach {
+                    builder.addStatement("%L", context.send(argument.value, it))
+                }
+                CodeBlock.of("%L.value", argument.value)
+            }
+            in receivingHosts -> {
+                val tempName1 = context.newTemporary("CommitTemp")
+                builder.addStatement(
+                    "val %N = %L",
+                    tempName1,
+                    context.receive((Committed::class).asTypeName().parameterizedBy(argType), source.cleartextHost),
+                )
+                val tempName2 = context.newTemporary("CommitTemp")
+                builder.addStatement(
+                    "val %N = %L",
+                    tempName2,
+                    argument.value,
+                )
+                val tempName3 = context.newTemporary("CommitTemp")
+                builder.addStatement(
+                    "val %N = %N.%N(%N)",
+                    tempName3,
+                    tempName2,
+                    "open",
+                    tempName1,
+                )
+
+                val peers = receivingHosts.filter { it != context.host }
+                if (peers.isNotEmpty()) {
+                    for (host in peers) builder.addStatement("%L", context.send(CodeBlock.of(tempName3), host))
+                    builder.addStatement(
+                        "%L",
+                        receiveExpected(
+                            CodeBlock.of(tempName3),
+                            context.host,
+                            argType,
+                            peers,
+                            context,
+                        ),
+                    )
+                }
+                CodeBlock.of("%N", tempName3)
+            }
+            else -> throw IllegalStateException()
+        }
+    }
+
     override fun import(
         protocol: Protocol,
         arguments: List<Argument>,
@@ -123,6 +251,13 @@ class CleartextCircuitCodeGenerator(context: CodeGeneratorContext) : AbstractCod
                 is Cleartext -> {
                     if (context.host in protocol.hosts + arg.protocol.hosts) {
                         move(arg.protocol, protocol, arg, builder)
+                    } else {
+                        CodeBlock.of("")
+                    }
+                }
+                is CommitmentProtocol -> {
+                    if (context.host in protocol.hosts + arg.protocol.hosts) {
+                        openCommitment(arg.protocol, protocol, arg, builder)
                     } else {
                         CodeBlock.of("")
                     }
@@ -145,6 +280,13 @@ class CleartextCircuitCodeGenerator(context: CodeGeneratorContext) : AbstractCod
                 is Cleartext -> {
                     if (context.host in protocol.hosts + arg.protocol.hosts) {
                         move(protocol, arg.protocol, arg, builder)
+                    } else {
+                        CodeBlock.of("")
+                    }
+                }
+                is CommitmentProtocol -> {
+                    if (context.host in protocol.hosts + arg.protocol.hosts) {
+                        createCommitment(protocol, arg.protocol, arg, builder)
                     } else {
                         CodeBlock.of("")
                     }
--- compiler/src/main/kotlin/io/github/aplcornell/viaduct/backends/commitment/CommitmentCircuitCodeGenerator.kt
+++ compiler/src/main/kotlin/io/github/aplcornell/viaduct/backends/commitment/CommitmentCircuitCodeGenerator.kt
@@ -1,207 +1,44 @@
 package io.github.aplcornell.viaduct.backends.commitment
 
 import com.squareup.kotlinpoet.CodeBlock
-import com.squareup.kotlinpoet.INT
-import com.squareup.kotlinpoet.MemberName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.TypeName
-import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.asTypeName
-import io.github.aplcornell.viaduct.backends.cleartext.Local
-import io.github.aplcornell.viaduct.backends.cleartext.Replication
 import io.github.aplcornell.viaduct.circuitcodegeneration.AbstractCodeGenerator
 import io.github.aplcornell.viaduct.circuitcodegeneration.Argument
 import io.github.aplcornell.viaduct.circuitcodegeneration.CodeGeneratorContext
 import io.github.aplcornell.viaduct.circuitcodegeneration.UnsupportedCommunicationException
-import io.github.aplcornell.viaduct.circuitcodegeneration.receiveExpected
 import io.github.aplcornell.viaduct.circuitcodegeneration.typeTranslator
 import io.github.aplcornell.viaduct.runtime.commitment.Committed
 import io.github.aplcornell.viaduct.syntax.Protocol
 import io.github.aplcornell.viaduct.syntax.types.ValueType
-import io.github.aplcornell.viaduct.runtime.commitment.Commitment as CommittmentValue
+import io.github.aplcornell.viaduct.runtime.commitment.Commitment as CommitmentValue
 
+/**
+ * Backend code generator for the commitment protocol for the circuit IR.
+ *
+ * Throws an UnsupportedCommunicationException when used in an input program as a computation protocol.
+ * This is because the commitment protocol is only a storage format and not a computation protocol.
+ */
 class CommitmentCircuitCodeGenerator(context: CodeGeneratorContext) : AbstractCodeGenerator(context) {
     override fun paramType(protocol: Protocol, sourceType: ValueType): TypeName {
         require(protocol is Commitment)
         return when (context.host) {
             protocol.cleartextHost -> (Committed::class).asTypeName().parameterizedBy(typeTranslator(sourceType))
-            in protocol.hashHosts -> (CommittmentValue::class).asTypeName().parameterizedBy(typeTranslator(sourceType))
-            else -> throw IllegalStateException()
-        }
-    }
-    override fun storageType(protocol: Protocol, sourceType: ValueType): TypeName = typeTranslator(sourceType)
-//    (CommittmentValue::class).asTypeName().parameterizedBy(typeTranslator(sourceType))
-
-    private fun createCommitment(
-        source: Protocol,
-        target: Protocol,
-        argument: Argument,
-        builder: CodeBlock.Builder,
-    ): CodeBlock {
-        require(context.host in source.hosts + target.hosts)
-        require(source is Local && source.hosts.size == 1 && source.host in source.hosts)
-        require(target is Commitment)
-        if (target.cleartextHost != source.host || target.cleartextHost in target.hashHosts) {
-            throw UnsupportedCommunicationException(source, target, argument.sourceLocation)
-        }
-        val argType = storageType(argument.protocol, argument.type)
-        val sendingHost = target.cleartextHost
-        val receivingHosts = target.hashHosts
-        return when (context.host) {
-            sendingHost -> {
-                val tempName1 = context.newTemporary("CommitTemp")
-                val tempName2 = context.newTemporary("CommitTemp")
-                builder.addStatement(
-                    "val %N = %T(%L)",
-                    tempName1,
-                    (Committed::class).asTypeName().parameterizedBy(argType),
-                    argument.value,
-                )
-                builder.addStatement(
-                    "val %N = %N.%M()",
-                    tempName2,
-                    tempName1,
-                    MemberName(Committed.Companion::class.asClassName(), "commitment"),
-                )
-                receivingHosts.forEach {
-                    builder.addStatement("%L", context.send(CodeBlock.of("%N", tempName2), it))
-                }
-                CodeBlock.of("%N", tempName1)
-            }
-
-            in receivingHosts -> {
-                val tempName3 = context.newTemporary("CommitTemp")
-                builder.addStatement(
-                    "val %N = %L",
-                    tempName3,
-                    context.receive((CommittmentValue::class).asTypeName().parameterizedBy(argType), source.host),
-                )
-                CodeBlock.of("%N", tempName3)
-            }
-
+            in protocol.hashHosts -> (CommitmentValue::class).asTypeName().parameterizedBy(typeTranslator(sourceType))
             else -> throw IllegalStateException()
         }
     }
 
-    private fun openCommitment(
-        source: Protocol,
-        target: Protocol,
-        argument: Argument,
-        builder: CodeBlock.Builder,
-    ): CodeBlock {
-        require(source is Commitment)
-        require(target is Replication)
-        require(context.host in source.hosts + target.hosts)
-        require(source.cleartextHost !in source.hashHosts)
-        if (source.hashHosts != target.hosts) {
-            throw UnsupportedCommunicationException(source, target, argument.sourceLocation)
-        }
-        val argType = storageType(argument.protocol, argument.type)
-        val receivingHosts = target.hosts
-        return when (context.host) {
-            source.cleartextHost -> {
-                receivingHosts.forEach {
-                    builder.addStatement("%L", context.send(argument.value, it))
-                }
-                CodeBlock.of("")
-            }
-            in receivingHosts -> {
-                val tempName1 = context.newTemporary("CommitTemp")
-                builder.addStatement(
-                    "val %N = %L",
-                    tempName1,
-                    context.receive((Committed::class).asTypeName().parameterizedBy(argType), source.cleartextHost),
-                    // TODO: Doubt: Don't really understand how this type parameter worked. Need to check
-                )
-                val tempName2 = context.newTemporary("CommitTemp")
-                builder.addStatement(
-                    "val %N = %L",
-                    tempName2,
-                    argument.value,
-                )
-                val tempName3 = context.newTemporary("CommitTemp")
-                builder.addStatement(
-                    "val %N = %N.%N(%N)",
-                    tempName3,
-                    tempName2,
-                    "open",
-                    tempName1,
-                )
-//                TODO: Need to do check that all receivers got the same value
-                val peers = receivingHosts.filter { it != context.host }
-                if (peers.isNotEmpty()) {
-                    for (host in peers) builder.addStatement("%L", context.send(CodeBlock.of(tempName3), host))
-                    builder.addStatement(
-                        "%L",
-                        receiveExpected(
-                            CodeBlock.of(tempName3),
-                            context.host,
-                            argType,
-                            peers,
-                            context,
-                        ),
-                    )
-                }
-                CodeBlock.of("%N", tempName3)
-            }
-            else -> throw IllegalStateException()
-        }
+    override fun storageType(protocol: Protocol, sourceType: ValueType): TypeName {
+        return super.storageType(protocol, sourceType)
     }
 
-    override fun import(
-        protocol: Protocol,
-        arguments: List<Argument>,
-    ): Pair<CodeBlock, List<CodeBlock>> {
-        require(protocol is Commitment)
-        val builder = CodeBlock.builder()
-        val values = arguments.map { arg ->
-            when (arg.protocol) {
-                is Local -> {
-                    if (context.host in protocol.hosts + arg.protocol.hosts) {
-                        createCommitment(arg.protocol, protocol, arg, builder)
-                    } else {
-                        CodeBlock.of("")
-                    }
-                }
-                is Commitment -> {
-                    if (context.host in protocol.hosts + arg.protocol.hosts) {
-                        builder.addStatement("%L", arg.value)
-                        arg.value
-                    } else {
-                        CodeBlock.of("")
-                    }
-                }
-
-                else -> throw UnsupportedCommunicationException(arg.protocol, protocol, arg.sourceLocation)
-            }
-        }
-        return Pair(builder.build(), values)
+    override fun import(protocol: Protocol, arguments: List<Argument>, ): Pair<CodeBlock, List<CodeBlock>> {
+        throw UnsupportedCommunicationException(arguments.first().protocol, protocol, arguments.first().sourceLocation)
     }
 
     override fun export(protocol: Protocol, arguments: List<Argument>): Pair<CodeBlock, List<CodeBlock>> {
-        require(protocol is Commitment)
-        val builder = CodeBlock.builder()
-        val values = arguments.map { arg ->
-            when (arg.protocol) {
-                is Commitment -> {
-                    if (context.host in protocol.hosts + arg.protocol.hosts) {
-                        builder.addStatement("%L", arg.value)
-                        arg.value
-                    } else {
-                        CodeBlock.of("")
-                    }
-                }
-                is Replication -> {
-                    openCommitment(protocol, arg.protocol, arg, builder)
-                }
-                else -> throw UnsupportedCommunicationException(arg.protocol, protocol, arg.sourceLocation)
-            }
-        }
-        return Pair(builder.build(), values)
+        throw UnsupportedCommunicationException(arguments.first().protocol, protocol, arguments.first().sourceLocation)
     }
 }
-
-// Import from local to commitment: If cleartextHost, create a commitment object and send. If hashHost, receive the commitment object
-// Export from commitment to commitment: Do nothing
-// Import from commitment to commitment: Do nothing
-// Export from commitment to Replication: If cleartextHost, send the secret. If hashHost, receive the secret and open the commitment object
